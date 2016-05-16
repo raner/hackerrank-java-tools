18634a457afa4c3c6875aa7243d85edf20f1a515
Mirko Raner <mirko@raner.ws>
2016-05-16 01:25:51 -0700

    added documentation

--- src/main/java/BinaryTest.java
+++ src/main/java/BinaryTest.java
@@ -1,10 +1,45 @@
+//                                                                          //
+// Copyright 2016 Mirko Raner                                               //
+//                                                                          //
+// Licensed under the Apache License, Version 2.0 (the "License");          //
+// you may not use this file except in compliance with the License.         //
+// You may obtain a copy of the License at                                  //
+//                                                                          //
+//     http://www.apache.org/licenses/LICENSE-2.0                           //
+//                                                                          //
+// Unless required by applicable law or agreed to in writing, software      //
+// distributed under the License is distributed on an "AS IS" BASIS,        //
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. //
+// See the License for the specific language governing permissions and      //
+// limitations under the License.                                           //
+//                                                                          //
+
 import java.io.ByteArrayOutputStream;
 import java.lang.reflect.Method;
 import java.security.SecureClassLoader;
 import java.util.Base64;
 
+//------------- Paste the code ABOVE THIS LINE into the HEAD section of your HackerRank test's code template ------------//
+
+//------------- Paste the code BELOW THIS LINE into the TAIL section of your HackerRank test's code template ------------//
+
+/**
+* The {@link BinaryTest} class reads a Base64-encoded Java {@code .class} file from {@link System#in} and loads and executes
+* the class defined by this file. The loaded class must have a {@code main} method.
+* Class files can be encoded using the standard {@code base64} UNIX command.
+*
+* @author Mirko Raner, all rights reserved
+**/
 public class BinaryTest
 {
+    /**
+    * Reads a Base64-encoded Java {@code .class} file from {@link System#in} and loads and executes it.
+    * The input consists of the fully qualified class name, a line-feed character (LF; ASCII 10) and the Base64-encoded
+    * class file (MIME encoding per RFC 2045).
+    * @param arguments the command-line arguments (not evaluated)
+    * @throws Exception if the code caused an {@link Error} or some other non-{@link Exception} {@link Throwable}
+    * (all regular {@link Exception}s will be caught by default and reported to {@link System#out})
+    **/
     public static void main(String... arguments) throws Exception
     {
         try
@@ -39,7 +74,7 @@ public class BinaryTest
             Class<?> testClass = classLoader.loadClass(name);
             Method main = testClass.getDeclaredMethod("main", String[].class);
             String[] noArguments = {};
-            main.invoke(null, (Object) noArguments);
+            main.invoke(null, (Object)noArguments);
         }
         catch (Exception exception)
         {
--- src/main/java/SourceTest.java
+++ src/main/java/SourceTest.java
@@ -1,14 +1,60 @@
-import java.io.*;
+//                                                                          //
+// Copyright 2016 Mirko Raner                                               //
+//                                                                          //
+// Licensed under the Apache License, Version 2.0 (the "License");          //
+// you may not use this file except in compliance with the License.         //
+// You may obtain a copy of the License at                                  //
+//                                                                          //
+//     http://www.apache.org/licenses/LICENSE-2.0                           //
+//                                                                          //
+// Unless required by applicable law or agreed to in writing, software      //
+// distributed under the License is distributed on an "AS IS" BASIS,        //
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. //
+// See the License for the specific language governing permissions and      //
+// limitations under the License.                                           //
+//                                                                          //
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintWriter;
 import java.net.URI;
 import java.security.SecureClassLoader;
 import java.text.MessageFormat;
 import java.util.concurrent.Callable;
-import javax.tools.*;
+import javax.tools.FileObject;
+import javax.tools.ForwardingJavaFileManager;
+import javax.tools.JavaCompiler;
+import javax.tools.JavaFileManager;
+import javax.tools.JavaFileObject;
+import javax.tools.SimpleJavaFileObject;
+import javax.tools.ToolProvider;
 import static java.util.Collections.singleton;
 import static javax.tools.JavaFileObject.Kind.SOURCE;
 
+//------------- Paste the code ABOVE THIS LINE into the HEAD section of your HackerRank test's code template ------------//
+
+//------------- Paste the code BELOW THIS LINE into the TAIL section of your HackerRank test's code template ------------//
+
+/**
+* The {@link SourceTest} class reads a Java source snippet from {@link System#in}, compiles it into a {@code .class} file,
+* and loads and executes it. As this mechanism relies on the {@link JavaCompiler}, a full JDK (as opposed to just a JRE)
+* is required at runtime.
+*
+* @author Mirko Raner, all rights reserved
+**/
 public class SourceTest
 {
+    /**
+    * Reads a Java source snippet from {@link System#in}, compiles it into a {@code .class} file, and loads and executes
+    * it. The snippet must be valid code when pasted into the {@link Callable#call} method of a {@link Callable} of
+    * {@link Void}. A terminal {@code return null;} statement is appended as part of the template; if the code needs to
+    * return early (i.e., there are additional return points in the code) this must be done by returning {@code null}.
+    * Compilation errors will be reported on {@link System#out}.
+    *
+    * @param arguments the command line arguments (not evaluated)
+    * @throws Exception if a compilation or runtime exception occurred
+    **/
     public static void main(String... arguments) throws Exception
     {
         String TEST_CLASS = "Test";
